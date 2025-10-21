d9182bbbc3737703d589c3bb90bf2049a8965b29
Lsantillan13 <lautarosantillann@icloud.com>
2025-10-21 03:48:36 -0300

    Last server config updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import app from './app.js';
+
 import './db.js'; // Import the database connection
 
-app.listen(4000, () => { console.log('Server is running on port 3000'); })
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); })
