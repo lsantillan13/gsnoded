2bf439a6f5c9d24342bd94e919bdb12ddde8d408
Lsantillan13 <lautarosantillann@icloud.com>
2025-12-10 17:09:20 -0300

    Fix product schema

--- src/models/Product.js
+++ src/models/Product.js
@@ -2,15 +2,13 @@ import { Schema, model } from 'mongoose';
 
 const productSchema = new Schema({
     name: { type: String, required: true },
-    price: { type: Number, required: false },
     description: { type: String, required: true },
     imageUrl: { type: String, required: true },
     images: { type: [String], required: true },
     category: { type: String, required: true },
-    fatherCategory: { type: String, required: true },
+    subcategory: { type: String, required: true },
+    isNew: { type: Boolean, default: false },
     brand: { type: String, required: false },
-    stock: { type: Number, required: false },
-    sku: { type: String, required: false, unique: true },
     createdAt: { type: Date, default: Date.now },
     productStatus: { type: Boolean, default: true},
     labelType: { type: [String], required: false }
